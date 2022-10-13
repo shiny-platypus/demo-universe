f74cfe0b8b584939e79f77a6ba0b916c6166a7f4
Guinevere Saenger <guinevere@pulumi.com>
2022-10-13 16:14:32 -0700

    Ready for demo

--- pulumi/main.go
+++ pulumi/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/pulumi/pulumi-github/sdk/v4/go/github"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"strconv"
 )
 
 type Member struct {
@@ -15,118 +13,80 @@ type Member struct {
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create a team
-		animalsTeam, err := github.NewTeam(ctx, "animals", &github.TeamArgs{
+		team, err := github.NewTeam(ctx, "animals", &github.TeamArgs{
 			Description: pulumi.String("Welcome to the Zoo"),
 			Name:        pulumi.String("animals"),
 			Privacy:     pulumi.String("closed"),
 		})
+
 		if err != nil {
 			return err
 		}
+		ctx.Export("team name", team.Name)
 
 		// Add members to animals team
 
-		guin := Member{UserName: "guineveresaenger"}
+		//teamMember, err := github.NewTeamMembership(ctx, "animals", &github.TeamMembershipArgs{
+		//	TeamId:   team.ID(),
+		//	Username: pulumi.String("guineveresaenger"),
+		//}, pulumi.Parent(team))
+		//if err != nil {
+		//	return err
+		//}
+		//ctx.Export("team member", teamMember.Username)
 
-		// unique name for TeamMembership
-		utmName := "animals" + "-" + guin.UserName
-		animalsTeamMember, err := github.NewTeamMembership(ctx, utmName, &github.TeamMembershipArgs{
-			TeamId:   animalsTeam.ID(),
-			Username: pulumi.String(guin.UserName),
-		})
+		// Create and initialize a repo
 
-		//guin := Member{UserName: "guineveresaenger"}
-		//
-		//// unique name for TeamMembership
-		//utmName := "animals" + "-" + guin.UserName
-		//animalsTeamMember, err := github.NewTeamMembership(ctx, utmName, &github.TeamMembershipArgs{
-		//	TeamId:   animalsTeam.ID(),
-		//	Username: pulumi.String(guin.UserName),
+		//testRepo, err := github.NewRepository(ctx, "test-repo", &github.RepositoryArgs{
+		//	Name:     pulumi.String("test-repo"),
+		//	AutoInit: pulumi.Bool(true),
 		//})
 		//
-		//guin := Member{UserName: "guineveresaenger"}
-		//
-		//// unique name for TeamMembership
-		//utmName := "animals" + "-" + guin.UserName
-		//animalsTeamMember, err := github.NewTeamMembership(ctx, utmName, &github.TeamMembershipArgs{
-		//	TeamId:   animalsTeam.ID(),
-		//	Username: pulumi.String(guin.UserName),
+		//if err != nil {
+		//	return err
+		//}
+		//// Set default branch to main
+		//_, err = github.NewBranchDefault(ctx, "main", &github.BranchDefaultArgs{
+		//	Branch:     pulumi.String("main"),
+		//	Repository: testRepo.Name,
 		//})
 		//
-		//guin := Member{UserName: "guineveresaenger"}
-		//
-		//// unique name for TeamMembership
-		//utmName := "animals" + "-" + guin.UserName
-		//animalsTeamMember, err := github.NewTeamMembership(ctx, utmName, &github.TeamMembershipArgs{
-		//	TeamId:   animalsTeam.ID(),
-		//	Username: pulumi.String(guin.UserName),
-		//})
-
-		// Create a sub-team
-		platypusTeam, err := github.NewTeam(ctx, "platypuses", &github.TeamArgs{
-			Description: pulumi.String("Duck-billed mammals"),
-			Name:        pulumi.String("platypuses"),
-			ParentTeamId: animalsTeam.ID().ApplyT(func(id interface{}) int {
-				x := fmt.Sprintf("%v", id) // we need the ID to be a real value, not an interface
-				y, _ := strconv.Atoi(x)
-				return y
-			}).(pulumi.IntOutput),
-			Privacy: pulumi.String("closed"),
-		},
-			pulumi.Parent(animalsTeam),
-		)
-
-		if err != nil {
-			return err
-		}
-
-		// Create a repo
-
-		testRepo, err := github.NewRepository(ctx, "test-repo", &github.RepositoryArgs{
-			Name:     pulumi.String("test-repo"),
-			AutoInit: pulumi.Bool(true),
-		}, pulumi.Protect(false))
-
-		_, err = github.NewBranchDefault(ctx, "main", &github.BranchDefaultArgs{
-			Branch:     pulumi.String("main"),
-			Repository: testRepo.Name,
-		})
+		//if err != nil {
+		//	return err
+		//}
+		//ctx.Export("reponame", testRepo.Name)
 
 		// Add repository permissions
 
-		perm := "admin"
-
-		_, err = github.NewTeamRepository(ctx, fmt.Sprintf("%s-%s-%s", testRepo, platypusTeam.Name, perm), &github.TeamRepositoryArgs{
-			Permission: pulumi.String(perm),
-			Repository: testRepo.Name,
-			TeamId:     platypusTeam.ID(),
-		}, pulumi.Protect(false))
-
-		if err != nil {
-			return err
-		}
+		//perm := "admin"
+		//teamRepoResourceName := "test-repo" + "-animals-" + perm
+		//teamRepo, err := github.NewTeamRepository(ctx, teamRepoResourceName, &github.TeamRepositoryArgs{
+		//	Permission: pulumi.String(perm),
+		//	Repository: testRepo.Name,
+		//	TeamId:     team.ID(),
+		//})
+		//
+		//if err != nil {
+		//	return err
+		//}
+		//ctx.Export("team repo permission", teamRepo.TeamId)
 
 		//Open PRs across a few pre-made repos, which will trigger an Action that is also pre-set-up.
+
 		//
-		managedRepos := []string{"development", "staging", "production"}
+		//managedRepos := []string{"development", "staging", "production"}
+		//for _, repo := range managedRepos {
+		//	err = createPR(ctx, repo)
+		//}
 
-		for _, repo := range managedRepos {
-			err = createPR(ctx, repo)
-		}
-
-		ctx.Export("pTeamName", platypusTeam.Name)
-		ctx.Export("aTeamMember", animalsTeamMember.Username)
-		//ctx.Export("reponame", testRepo.Name)
-		//ctx.Export("rteam repo permission", teamRepo.TeamId)
-		//ctx.Export("pull request number", pr.Number)
 		return nil
 	})
 
 }
 
 func createPR(ctx *pulumi.Context, repoName string) error {
-	branchName := repoName + "-feature-branch"
-	featureBranch, err := github.NewBranch(ctx, branchName, &github.BranchArgs{
+	branchResourceName := repoName + "-feature-branch"
+	featureBranch, err := github.NewBranch(ctx, branchResourceName, &github.BranchArgs{
 		Branch:     pulumi.String("new-feature"),
 		Repository: pulumi.String(repoName),
 	})
@@ -134,8 +94,8 @@ func createPR(ctx *pulumi.Context, repoName string) error {
 		return err
 	}
 
-	fileName := repoName + "-contributing"
-	contributing, err := github.NewRepositoryFile(ctx, fileName, &github.RepositoryFileArgs{
+	fileResourceName := repoName + "-contributing"
+	contributing, err := github.NewRepositoryFile(ctx, fileResourceName, &github.RepositoryFileArgs{
 		Content:    pulumi.String("Remember - every repository should have a contributor's guide."),
 		File:       pulumi.String("CONTRIBUTING.md"),
 		Repository: pulumi.String(repoName),
@@ -147,8 +107,8 @@ func createPR(ctx *pulumi.Context, repoName string) error {
 	}
 
 	//let's open a PR!
-	prName := repoName + "-feature-pr"
-	_, err = github.NewRepositoryPullRequest(ctx, prName, &github.RepositoryPullRequestArgs{
+	prResourceName := repoName + "-feature-pr"
+	_, err = github.NewRepositoryPullRequest(ctx, prResourceName, &github.RepositoryPullRequestArgs{
 		BaseRef:        pulumi.String("main"),
 		BaseRepository: pulumi.String(repoName),
 		HeadRef:        featureBranch.Branch,
@@ -158,34 +118,6 @@ func createPR(ctx *pulumi.Context, repoName string) error {
 
 	if err != nil {
 		return err
-	}
-	return nil
-}
-
-func addMembers(ctx *pulumi.Context, members []Member, team *github.Team, teamName string) error {
-	for _, member := range members {
-		// unique name for TeamMembership
-		utmName := teamName + "-" + member.UserName
-		// set up team maintainers if that Role is set
-		if member.Role != "" {
-			_, err := github.NewTeamMembership(ctx, utmName, &github.TeamMembershipArgs{
-				TeamId:   team.ID(),
-				Username: pulumi.String(member.UserName),
-				Role:     pulumi.String(member.Role),
-			})
-			if err != nil {
-				return err
-			}
-		} else { // default to non-maintainer team membership
-			_, err := github.NewTeamMembership(ctx, utmName, &github.TeamMembershipArgs{
-				TeamId:   team.ID(),
-				Username: pulumi.String(member.UserName),
-			})
-			if err != nil {
-				return err
-			}
-		}
-
 	}
 	return nil
 }
